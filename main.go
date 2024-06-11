2774653788e08e1fb9cb33c7d99860dfb7be907a
OsauravO <saurav1hasyo@gmail.com>
2024-06-11 07:08:37 +0530

    feat: hashing logic added

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+
+)
+
 type BlockHeader struct {
 	Version     uint32
 	PrblockHash string
@@ -15,9 +21,6 @@ type Input struct {
 	Prevout      Prevout  `json:"prevout"`
 	Scriptsig    string   `json:"scriptsig"`
 	ScriptsigAsm string   `json:"scriptsig_asm"`
-	Witness      []string `json:"witness"`
-	IsCoinbase   bool     `json:"is_coinbase"`
-	Sequence     uint32   `json:"sequence"`
 }
 
 type Prevout struct {
@@ -35,3 +38,29 @@ type Transaction struct {
 	Vout     []Prevout `json:"vout"`
 }
 
+func proofOfWork(blockHeader *BlockHeader) bool {
+	
+}
+
+func serTx(tx *Transaction) []byte {
+	
+}
+
+func srlzBhead(bh *BlockHeader) []byte {
+	
+}
+
+func calculateTxID(serializedTx []byte) string {
+	hash := doubleHash(serializedTx)
+	reversedHash := rb(hash)
+	return hex.EncodeToString(reversedHash)
+}
+
+func doubleHash(data []byte) []byte {
+	return sha256h(sha256h(data))
+}
+
+func sha256h(data []byte) []byte {
+	hash := sha256.Sum256(data)
+	return hash[:]
+}
